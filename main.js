ded115962323f9f9b38cf4dbe83f91688a7a8b02
SamsTheNerd <sam@samsthenerd.com>
2023-11-03 22:20:24 -0400

    projects are kinda buttony

--- main.js
+++ main.js
@@ -23,9 +23,9 @@ var genFakeProjectsData = () => {
 }
 
 // returns string of html for a single project card -- only 
-var genPreviewCard = (project) => {
+var genPreviewCard = (project, index) => {
     var htmlString = `
-        <div class="project-card">
+        <div class="project-card" id="project-card-${index}" onclick="setActiveProject(${index})">
             <img src="${project.image}"/>
             <h3 class="project-card-title">${project.title}</h3>
         </div>
@@ -36,7 +36,43 @@ var genPreviewCard = (project) => {
 window.onload = () => {
     var projectsData = genFakeProjectsData();
     var projectsContainer = document.getElementById("projects-container");
+    var i = 0;
     projectsData.forEach((project) => {
-        projectsContainer.innerHTML += genPreviewCard(project);
+        projectsContainer.innerHTML += genPreviewCard(project, i);
+        i++;
     });
+}
+
+var activeProject = -1;
+
+var setActiveProject = (index) => {
+    var detailView = document.getElementById("project-details");
+    if(activeProject != -1){
+        // unactivate old one
+        var oldActive = document.getElementById("project-card-" + activeProject);
+        oldActive.classList.remove("active-project");
+        detailView.classList.remove("visible");
+        if(activeProject == index){
+            document.body.style.setProperty("--active-project", -1);
+            activeProject = -1;
+            return;
+        }
+    }
+    var newActive = document.getElementById("project-card-" + index);
+    newActive.classList.add("active-project");
+    detailView.classList.add("visible");
+    document.body.style.setProperty("--active-project", index);
+    activeProject = index;
+    updateDetailPosition();
+}
+
+var updateDetailPosition = () => {
+    var detailView = document.getElementById("project-details");
+    var projectColumns = getComputedStyle(document.body).getPropertyValue("--project-columns");
+    var projectRow = Math.floor(activeProject / projectColumns) + 2;
+    detailView.style.setProperty("grid-row", projectRow);
+}
+
+window.onresize = () => {
+    updateDetailPosition();
 }
